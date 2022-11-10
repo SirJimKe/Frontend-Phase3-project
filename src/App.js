5ef35c4e153bef4a100e9f9b7ccf1c82ee15754e
SirJimKe <jimmymutuku1@gmail.com>
2022-11-10 09:30:43 +0300

    Header and Navbar functions added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Switch, Route} from "react-router-dom";
 import Header from './Components/Header';
 import './App.css';
@@ -5,23 +6,42 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Songs from './Components/Songs';
 import Favorites from './Components/Favorites';
+import MusicDetails from './Components/MusicDetails';
+import AddMusic from './Components/AddMusic';
 
 function App() {
 
-    return (
+  const [songs, setSongs] = useState([]);
+  const [show, setShow] = useState(false);
+  const [endPoint, setEndPoint] = useState('');
+
+
+  return (
     <div className="App">
-      <Header />
-      <Navbar/>
+      <Header 
+        endPoint={endPoint}
+        onChangeHandler= {onChangeHandler}
+        submitHandler={submitHandler} 
+        show={show}
+        setShow={setShow} 
+      />
+      <Navbar setShow={setShow} />
 
       <Switch>
         <Route exact path="/" >
           <Home />
         </Route>
-        <Route path="/songs" >
-          <Songs />
+        <Route exact path="/songs" >
+          <Songs songs={songs} updateFavorite={updateFavorite} />
+        </Route>
+        <Route path="/songs/:id" >
+          <MusicDetails deleteSong={deleteSong} />
         </Route>
         <Route path="/favorites">
-          <Favorites />
+          <Favorites songs={songs.filter(song=> song.favorite)} updateFavorite={updateFavorite} />
+        </Route>
+        <Route path="/addmusic" >
+          <AddMusic />
         </Route>
       </Switch>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = ({setShow}) => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <NavLink to="/songs" onClick={()=>setShow(true)} >Songs</NavLink>
+      <NavLink to="/favorites" onClick={()=>setShow(true)}  >Favorites</NavLink>
+      <NavLink to="/addmusic" onClick={()=>setShow(false)}  >Add Music</NavLink>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
