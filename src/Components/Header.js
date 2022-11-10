c334c14fa9275c0bc3d9efb4280859dea9d90b4a
SirJimKe <jimmymutuku1@gmail.com>
2022-11-10 07:53:43 +0300

    Worked on the Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Searchbar from "./Searchbar";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+    return (
+        <header className="header">
+            <div className="header_logo">           
+                <NavLink exact to="/" >Muziki</NavLink>
+            </div>
+            <Searchbar />
+            <h2><NavLink exact to="/" >Home</NavLink></h2>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
